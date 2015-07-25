14f9fa854db3661c98ee4de07b1f21f5960fa973
Mario Gonzalez <necrillo@gmail.com>
2015-07-25 13:32:24 -0300

    Estructura inicial

---
+++ asset/js/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('expenseApp',[])
+ 
+app.controller('HomeViewController',['$scope',function($scope){
+	$scope.appTitle = 'Simple Expenses Tracker';
+}]);
+
+app.controller('ExpensesViewController',['$scope',function($scope){
+	$scope.expenses = [
+		{description: 'food', amount: 10, date: '01-10-2014'},
+		{description: 'tickets', amount: 11, date: '02-10-2014'},
+		{description: 'food', amount: 12, date: '03-10-2014'},
+		{description: 'phone credit', amount: 13, date: '04-10-2014'},
+		{description: 'bills', amount: 14, date: '05-10-2014'},
+		{description: 'food', amount: 15, date: '06-10-2014'},
+	]
+}]);
